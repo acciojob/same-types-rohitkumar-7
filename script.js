bb7aa7fecb7f4d298b9e15f3d4832a3cbdad5899
Rohit kumar <82631765+rohitkumar-7@users.noreply.github.com>
2025-04-18 13:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function isSameType(value1, value2) {
   //your js code here
+	let status = true;
+		if(typeof value1 == typeof value2){
+				status=true;
+		}else{
+			status= flase;
+		}
+		return status;
 }
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
